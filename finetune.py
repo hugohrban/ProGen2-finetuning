8e852ef656e05e0f7f497c32d06831c52ea28910
Hugo Hrbáň <hugohrban2@gmail.com>
2024-04-23 23:08:20 +0200

    cleanup finetuning script

--- finetune.py
+++ finetune.py
@@ -14,6 +14,10 @@ from transformers import (
     get_constant_schedule_with_warmup,
 )
 from tqdm import tqdm
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+logger = logging.getLogger(__name__)
 
 
 class Protein_dataset(Dataset):
@@ -29,7 +33,7 @@ class Protein_dataset(Dataset):
         line = self.tokenizer.encode(line)
         return torch.tensor(line.ids)
 
-    
+
 def load_data(file: str) -> tuple[list[str], list[str]]:
     lines = []
     prefixes = set()
@@ -44,31 +48,26 @@ def load_data(file: str) -> tuple[list[str], list[str]]:
 
 
 def init_new_embeddings(model: ProGenForCausalLM, prefixes: list[str]):
-    print("initializing new embeddings")
-    #assert len(prefixes) >= 2, "must have at least 2 new embeddings"
     if len(prefixes) <= 2:
-        print("No new embeddings to initialize")
+        logger.info("No new embeddings to initialize.")
         return
     new_embs = torch.zeros((len(prefixes) - 2, model.config.n_embd)).to(model.device)
-    
+
     unk_token_emb: torch.Tensor = model.transformer.wte.weight[-1].detach()
     mean_unk_emb = torch.zeros_like(new_embs) + unk_token_emb.mean()
     std_unk_emb = torch.zeros_like(new_embs) + unk_token_emb.std()
-    # print("unk token emb", unk_token_emb)
-    # print("mean_unk_emb", mean_unk_emb)
-    # print("std_unk_emb", std_unk_emb)
 
     # initialize new embeddings with normal distribution same as untrained embeddings
     torch.normal(mean_unk_emb, std_unk_emb, out=new_embs)
     new_embs = torch.cat([model.transformer.wte.weight, new_embs], dim=0)
+    logger.debug(f"New embeddings shape: {new_embs.shape}")
     model.transformer.wte.weight = torch.nn.Parameter(new_embs, requires_grad=True)
     model.config.vocab_size_emb = new_embs.shape[0]
-    
-#    print(new_embs)
-#    print("new embs shape", new_embs.shape)
 
 
-def get_lr_schedule(optimizer, args, train_steps):
+def get_lr_schedule(
+    optimizer: torch.optim.Optimizer, args: argparse.Namespace, train_steps: int
+):
     if args.decay == "cosine":
         scheduler = get_cosine_schedule_with_warmup(
             optimizer,
@@ -91,11 +90,20 @@ def get_lr_schedule(optimizer, args, train_steps):
             num_warmup_steps=args.warmup_steps,
         )
     else:
-        raise ValueError(f"Invalid learning rate decay type. Must be 'cosine', 'linear', or 'constant'. Got: {args.decay}")
+        raise ValueError(
+            f"Invalid learning rate decay type. Must be 'cosine', 'linear', 'exponential', or 'constant'. Got: {args.decay}"
+        )
     return scheduler
 
 
-def train_epoch(model: ProGenForCausalLM, dataset: Protein_dataset, optimizer, scheduler, epoch, args, eval_dataset=None):
+def train_epoch(
+    model: ProGenForCausalLM,
+    dataset: Protein_dataset,
+    optimizer: torch.optim.Optimizer,
+    scheduler: torch.optim.lr_scheduler.LambdaLR,
+    epoch: int,
+    args: argparse.Namespace,
+):
     model.train()
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
     total_loss = 0
@@ -112,19 +120,19 @@ def train_epoch(model: ProGenForCausalLM, dataset: Protein_dataset, optimizer, s
             scheduler.step()
             optimizer.zero_grad()
             pbar.update()
-            #print(f"\rTRAIN step: {i // args.accumulation_steps} / {len(dataloader) // args.accumulation_steps}: loss: {loss.item()}", end="")
-        # evaluate on test set in the middle of training epoch
-        if eval_dataset is not None and i == len(dataloader) // 2:
-            print(f"Running test set evaluation after {epoch}.5 epochs...")
-            evaluate(model, eval_dataset, args)
     pbar.close()
-    print(f"\nTRAIN epoch {epoch}: loss: {total_loss / len(dataloader)}")
-    print(f"Last learning rate: {scheduler.get_last_lr()}")
+    logger.info(f"\nTRAIN epoch {epoch}: loss: {total_loss / len(dataloader)}")
+    logger.debug(f"Last learning rate: {scheduler.get_last_lr()}")
     return total_loss / len(dataloader)
 
 
 @torch.no_grad()
-def evaluate(model, dataset, args, before_train=False):
+def evaluate(
+    model: ProGenForCausalLM,
+    dataset: Protein_dataset,
+    args: argparse.Namespace,
+    before_train: bool = False,
+):
     model.eval()
     total_loss = 0
     if before_train:
@@ -132,60 +140,57 @@ def evaluate(model, dataset, args, before_train=False):
         dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
     else:
         dataloader = DataLoader(dataset, batch_size=args.batch_size * 4, shuffle=True)
-    total_length = len(dataloader) if not before_train else 5000
+    total_length = len(dataloader)
     pbar = tqdm(total=total_length)
-    for i, batch in enumerate(dataloader):
+    for batch in dataloader:
         if before_train:
-            # truncate padding, because the base model wasn't trained with padding
+            # remove padding, because the base model wasn't trained with padding
             non_zero_length = (batch != 0).sum().item()
             batch = batch[:, :non_zero_length]
-            if i == 5000:
-                break
         batch = batch.to(args.device)
         loss = model(batch, labels=batch).loss
         total_loss += loss.item()
         pbar.update()
-        #print(f"\rEVAL step: {i} / {total_length}: loss: {loss.item()}", end="")
     pbar.close()
-    print(f"\nEVAL loss: {total_loss / total_length}")
+    logger.info(f"EVAL loss: {total_loss / total_length}")
     return total_loss / total_length
 
 
 def train(
-    model,
-    tokenizer, 
-    train_dataset,
-    test_dataset,
-    optimizer,
-    scheduler,
-    args,
-    job_id,
+    model: ProGenForCausalLM,
+    tokenizer: Tokenizer,
+    train_dataset: Protein_dataset,
+    test_dataset: Protein_dataset,
+    optimizer: torch.optim.Optimizer,
+    scheduler: torch.optim.lr_scheduler.LambdaLR,
+    args: argparse.Namespace,
+    job_id: str,
 ):
     train_losses = []
     eval_losses = []
     for epoch in range(1, args.epochs + 1):
-        print(f"\n=========================")
-        print(f"Start time of epoch {epoch}: {datetime.now()}")
+        logger.info(f"Start time of epoch {epoch}: {datetime.now()}")
         train_loss = train_epoch(model, train_dataset, optimizer, scheduler, epoch, args)
         train_losses.append(train_loss)
-        print(f"\nRunning test set evaluation after {epoch} epochs:")
+        logger.info(f"\nRunning test set evaluation after {epoch} epochs:")
         eval_loss = evaluate(model, test_dataset, args)
         eval_losses.append(eval_loss)
         model_name = job_id + "-" + args.model.strip("/").split("/")[-1]
         if epoch % args.checkpoint_rate == 0 or epoch == args.epochs:
             dir_path = f"./checkpoints/{model_name}-finetuned/e{epoch}/"
             os.makedirs(dir_path, exist_ok=True)
-            print(f"Saving model, optimizer, and scheduler at epoch {epoch}...")
-            # torch.save(model.state_dict(), dir_path + "pytorch_model.bin")
             model.save_pretrained(dir_path)
             tokenizer.save(os.path.join(dir_path, "tokenizer.json"), pretty=True)
             if args.save_optimizer:
-                torch.save(optimizer.state_dict(), os.path.join(dir_path, "optimizer.pt"))
-                torch.save(scheduler.state_dict(), os.path.join(dir_path, "scheduler.pt"))
-            print(f"Model saved at: {dir_path}")
+                logger.info("Saving optimizer and scheduler...")
+                torch.save(
+                    optimizer.state_dict(), os.path.join(dir_path, "optimizer.pt")
+                )
+                torch.save(
+                    scheduler.state_dict(), os.path.join(dir_path, "scheduler.pt")
+                )
+            logger.info(f"Model saved at: {dir_path}")
     return model, train_losses, eval_losses
-
-
 
 
 def main(args: argparse.Namespace):
@@ -193,14 +198,13 @@ def main(args: argparse.Namespace):
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
 
-    #print("Finetuning start time:", datetime.now())
-
     job_id = os.environ.get("SLURM_JOB_ID")
     if job_id is not None:
-        print(f"slurm job id: {job_id}")
+        logger.debug(f"Slurm job id: {job_id}")
     else:
-        print("no slurm job id found")
+        logger.warning("No Slurm job ID found.")
 
+    # loading data and tokenizer
     tokenizer: Tokenizer = Tokenizer.from_file("tokenizer.json")
     tokenizer.enable_padding(
         direction="right", pad_id=0, pad_token="<|pad|>", length=1024
@@ -209,67 +213,45 @@ def main(args: argparse.Namespace):
 
     train_data, prefixes = load_data(args.train_file)
     test_data, prefixes_test = load_data(args.test_file)
-
-    print("Found prefixes:", prefixes)
-    #print("Test prefixes", prefixes_test)
+    logger.info("Found prefixes:", prefixes)
     assert prefixes == prefixes_test, "Prefixes in train and test data must be the same"
-
-    # print(train_data[0])
     tokenizer.add_tokens(prefixes)
+
     train_data = Protein_dataset(train_data, tokenizer)
     test_data = Protein_dataset(test_data, tokenizer)
+    logger.debug("Train data size:", len(train_data))
+    logger.debug("Test data size:", len(test_data))
 
-    print(train_data[0])
-    print("Train data size:", len(train_data))
-    print("Test data size:", len(test_data))
-    
-    # return 0
+    if args.device == "cuda" and not torch.cuda.is_available():
+        logger.warning("CUDA not available. Falling back to CPU. Please consider using a GPU for training.")
+        device = torch.device("cpu")
+    else:
+        device = torch.device(args.device)
+    logger.info(f"Device: {device}")
 
-    # prefixes = list(prefixes)
-    # tokenizer.add_tokens(prefixes)
-    # print("prefixes:", prefixes)
-    #print("Tokenizer vocab:", tokenizer.get_vocab())
-    #print("Tokenizer vocab size:", tokenizer.get_vocab_size())
+    logger.debug(f"hyperparameters: effective batch={args.batch_size * args.accumulation_steps}, {args.batch_size=}, {args.accumulation_steps=}, {args.epochs=}, {args.lr=}, {args.warmup_steps=}, {args.checkpoint_rate=}")
 
-    
-    # return 0
-    
-    # saving the expanded tokenizer
-    # tokenizer.save("tokenizer_expanded.json", pretty=True)
-    # return 0
-
-
-    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    print(
-        f"hyperparameters: effective batch={args.batch_size * args.accumulation_steps}, {args.batch_size=}, {args.accumulation_steps=}, {args.epochs=}, {args.lr=}, {args.warmup_steps=}, {args.checkpoint_rate=}"
-    )
-
-    print(f"Using device: {args.device}")
-    print(f"Loading model: {args.model}...")
+    # loading model
+    logger.info(f"Loading model: {args.model}...")
     model = ProGenForCausalLM.from_pretrained(args.model).to(args.device)
-    print("Model loaded")
-    print(f"Model parameters: {model.num_parameters() // 1e6} M")
+    logger.info(f"Model loaded. Model parameter count: {model.num_parameters() // 1e6} M")
     init_new_embeddings(model, prefixes)
-    
-    
-    # print(model.config)
-    # model.save_pretrained("new_config_dummy_model")
-    # return 0
 
+    # creating optimizer and scheduler
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
     training_steps = (
         args.epochs * len(train_data) // (args.batch_size * args.accumulation_steps)
     )
     if training_steps > 0:
-        print("Weight updates per epoch", training_steps / args.epochs)
-    print("Total weight updates:", training_steps)
+        logger.debug("Weight updates per epoch", training_steps / args.epochs)
+    logger.debug("Total weight updates:", training_steps)
     scheduler = get_lr_schedule(optimizer, args, training_steps)
 
     if args.eval_before_train:
-        print("Runnning evaluation on test set before training...")
+        logger.info("Runnning evaluation on test set before training...")
         evaluate(model, test_data, args, before_train=True)
 
+    # training loop
     model, train_losses, test_losses = train(
         model,
         tokenizer,
@@ -280,45 +262,87 @@ def main(args: argparse.Namespace):
         args,
         job_id,
     )
-    print("Finetuning finished.")
-    print("Train losses", train_losses)
-    print("Test losses", test_losses)
+
+    logger.info("Finetuning finished.")
+    logger.info("Train losses", train_losses)
+    logger.info("Test losses", test_losses)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model",
         type=str,
-        default="./checkpoints/progen2-small",
-        help="Path to the model checkpoint to be finetuned.",
+        default="hugohrban/progen2-small",
+        help="Name of the model checkpoint to be finetuned.",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda",
+        help="Device to run on. Default: \"cuda\"",
     )
     parser.add_argument(
         "--train-file",
         type=str,
         required=True,
-        help="Path relative to ~/progen/progen2. Must contain preprocessed data (includes prefixes and one protein per line, e.g. not fasta format).",
+        help="Path to training data file. Must contain preprocessed data (includes prefixes and one protein per line, e.g. not fasta format).",
     )
     parser.add_argument(
         "--test-file",
         type=str,
         required=True,
-        help="Path relative to ~/progen/progen2. Must contain preprocessed data (includes prefixes and one protein per line, e.g. not fasta format).",
+        help="Path to test data file. Must contain preprocessed data (includes prefixes and one protein per line, e.g. not fasta format).",
     )
     parser.add_argument("--seed", type=int, default=69)
     parser.add_argument("--batch-size", type=int, default=16)
-    parser.add_argument("--accumulation-steps", type=int, default=32)
+    parser.add_argument(
+        "--accumulation-steps",
+        type=int,
+        default=32,
+        help="How many steps to accumulate gradients before updating weights. Default: 32",
+    )
     parser.add_argument("--epochs", type=int, default=5)
-    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate. Check out also the '--decay' argument. Default: 1e-4")
-    parser.add_argument("--warmup-steps", type=int, default=200)
     parser.add_argument(
-        "--checkpoint-rate", type=int, default=5, help="save checkpoint every n epochs"
+        "--lr",
+        type=float,
+        default=1e-4,
+        help="Learning rate. Check out also the '--decay' argument. Default: 1e-4",
     )
     parser.add_argument(
-        "--decay", type=str, choices=["cosine", "linear", "constant"], default="cosine", help="Learning rate decay. Default: 'cosine'"
+        "--warmup-steps",
+        type=int,
+        default=200,
+        help="Number of warmup steps for learning rate scheduler. Linearly increasing form 0 to --lr. Default: 200",
     )
-    parser.add_argument("--save-optimizer", action="store_true", default=False, help="Should we also save the optimizer (and scheduler) at every checkpoint")
-    parser.add_argument("--eval-before-train", action="store_true", default=False, help="Run evaluation on test set before training")
+    parser.add_argument(
+        "--checkpoint-rate", type=int, default=5, help="Save model checkpoint every n epochs. Default: 5"
+    )
+    parser.add_argument(
+        "--decay",
+        type=str,
+        choices=["cosine", "linear", "constant"],
+        default="cosine",
+        help="Learning rate decay. Default: \"cosine\"",
+    )
+    parser.add_argument(
+        "--save-optimizer",
+        action="store_true",
+        default=False,
+        help="Should we also save the optimizer and scheduler at every checkpoint",
+    )
+    parser.add_argument(
+        "--eval-before-train",
+        action="store_true",
+        default=False,
+        help="Run evaluation on test set before training. default: False",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        default=False,
+        help="Enable debug logging level.",
+    )
     args = parser.parse_args()
 
     main(args)
-
